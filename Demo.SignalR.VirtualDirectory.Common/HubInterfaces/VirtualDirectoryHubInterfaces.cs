849ec5a7eb1153f41457dd2915bb7262d7251444
Borcze Angelov <borcze.angelov@yahoo.com>
2020-07-20 23:25:26 +0200

    Demo.SignalR.VirtualDirectory.Common: Add VirtualDirectoryHubInterfaces.cs

---
+++ Demo.SignalR.VirtualDirectory.Common/HubInterfaces/VirtualDirectoryHubInterfaces.cs
@@ -0,0 +1,47 @@
+﻿using Demo.SignalR.VirtualDirectory.Common.DataModel;
+using System;
+using System.Threading.Tasks;
+
+namespace Demo.SignalR.VirtualDirectory.Common.HubInterfaces
+{
+    public interface IVirtualDirectoryHub
+    {
+        Task CreateFile();
+        Task UpdateFile(File file);
+        Task DeleteFile(File file);
+
+        Task CreateFolder();
+        Task UpdateFolder(Folder folder);
+        Task DeleteFolder(Folder folder);
+    }
+
+    public interface IVirtualDirectoryHubClient
+    {
+        void InvokeCreateFile(File file);
+        void InvokeUpdateFile(File file);
+        void InvokeDeleteFile(File file);
+
+        void InvokeCreateFolder(Folder folder);
+        void InvokeUpdateFolder(Folder folder);
+        void InvokeDeleteFolder(Folder folder);
+
+        //void InvokeLoadStartingValues();
+    }
+
+    public interface IVirtualDirectoryHubClientTwoWayComm : IVirtualDirectoryHubClient
+    {
+        event Action<File> FileCreated;
+        event Action<File> FileUpdated;
+        event Action<File> FileDeleted;
+
+        event Action<Folder> FolderCreated;
+        event Action<Folder> FolderUpdated;
+        event Action<Folder> FolderDeleted;
+
+        //event Action<> StartingValuesLoaded;
+
+        IVirtualDirectoryHub ServerHub { get; }
+
+        Task ConnectWithServerHub();
+    }
+}
