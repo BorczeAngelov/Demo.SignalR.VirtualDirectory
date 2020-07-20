3c27757434898a6834621b5dad0812b52ba7af2d
Borcze Angelov <borcze.angelov@yahoo.com>
2020-07-20 23:15:13 +0200

    Add basic data model classes: "File" and "Folder"

---
+++ Demo.SignalR.VirtualDirectory.Common/DataModel/Folder.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Demo.SignalR.VirtualDirectory.Common.DataModel
+{
+    public class Folder
+    {
+        public string Name { get; set; }
+        public Guid ObjectKey { get; set; }
+        public Guid ParentFolderObjectKey { get; set; }
+    }
+}
