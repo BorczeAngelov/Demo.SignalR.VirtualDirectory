fe8ec76171aeb3d276a1f8d9c81ef96d1d5a8626
Borcze Angelov <borcze.angelov@yahoo.com>
2020-07-20 23:05:24 +0200

    Add basic wpf utils classes

---
+++ Demo.SignalR.VirtualDirectory.Client.WPF/Utils/ObservableBase.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Demo.SignalR.VirtualDirectory.Client.WPF.Utils
+{
+    abstract internal class ObservableBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        internal void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
