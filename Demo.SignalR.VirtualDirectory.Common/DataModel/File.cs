06b55d42aec533fad088108c2a3d3d156080e4a9
Borcze Angelov <borcze.angelov@yahoo.com>
2020-07-21 22:48:57 +0200

    Simplify the "File DataModel"

--- Demo.SignalR.VirtualDirectory.Common/DataModel/File.cs
+++ Demo.SignalR.VirtualDirectory.Common/DataModel/File.cs
@@ -4,15 +4,7 @@ namespace Demo.SignalR.VirtualDirectory.Common.DataModel
 {
     public class File
     {
-        public enum FileType
-        {
-            txt,
-            png,
-            mp4
-        }
-
         public string Name { get; set; }
-        public FileType Type { get; set; }
         public Guid ObjectKey { get; set; }
         public Guid FolderObjectKey { get; set; }
     }
