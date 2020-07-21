c23212fef615dfaad950a411f0dac3df87a4b70f
Borcze Angelov <borcze.angelov@yahoo.com>
2020-07-21 20:40:10 +0200

    Create "Template selector" logic for based upon DataContext type

---
+++ Demo.SignalR.VirtualDirectory.Client.WPF/View/EditItem/EditItemView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace Demo.SignalR.VirtualDirectory.Client.WPF.View.EditItem
+{
+    public partial class EditItemView : UserControl
+    {
+        public EditItemView()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- Demo.SignalR.VirtualDirectory.Client.WPF/ViewModel/EditableItemBaseVM.cs
+++ Demo.SignalR.VirtualDirectory.Client.WPF/ViewModel/EditableItemBaseVM.cs
@@ -1,9 +1,10 @@
 ﻿using Demo.SignalR.VirtualDirectory.Client.WPF.Utils;
+using System;
 
 namespace Demo.SignalR.VirtualDirectory.Client.WPF.ViewModel
 {
     public abstract class EditableItemBaseVM : ObservableBase
     {
-
+        public Type Type { get => GetType(); }
     }
 }
