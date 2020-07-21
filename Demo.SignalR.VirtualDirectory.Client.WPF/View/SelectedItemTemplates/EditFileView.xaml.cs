74767b9f2543b06183c211f9fa9e5b7e745aeedf
Borcze Angelov <borcze.angelov@yahoo.com>
2020-07-21 22:49:11 +0200

    Frontend refactoring

---
+++ Demo.SignalR.VirtualDirectory.Client.WPF/View/SelectedItemTemplates/EditFileView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace Demo.SignalR.VirtualDirectory.Client.WPF.View.SelectedItemTemplates
+{
+    public partial class EditFileView : UserControl
+    {
+        public EditFileView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Demo.SignalR.VirtualDirectory.Client.WPF/View/SelectedItemTemplates/EditFolderView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace Demo.SignalR.VirtualDirectory.Client.WPF.View.SelectedItemTemplates
+{
+    public partial class EditFolderView : UserControl
+    {
+        public EditFolderView()
+        {
+            InitializeComponent();
+        }
+    }
+}
